bbd307fc023c37f3fe537dc9cb1a86b8f035cd71
estpha <esther.pham01@gmail.com>
2023-06-09 20:50:47 +0200

    ajout
    ajout du projet

---
+++ projet/projet/js/httpService.js
@@ -0,0 +1,35 @@
+/*
+ * Couche de services HTTP (worker). 
+ *
+ * @author Jean-Claude Stritt / modif P-A Mettraux
+ */
+class HttpService {
+  constructor() {}
+
+  /*
+  **  $.ajaxSetup permet de définir une fois un élément sans le refaire par la suite. Ici cela se fait l'error
+  */
+  centraliserErreurHttp(httpErrorCallbackFn) {
+    $.ajaxSetup({
+      error: function (xhr, exception) {
+        let msg;
+        if (xhr.status === 0) {
+          msg = "Pas d'accès à la ressource serveur demandée !";
+        } else if (xhr.status === 404) {
+          msg = "Page demandée non trouvée [404] !";
+        } else if (xhr.status === 500) {
+          msg = "Erreur interne sur le serveur [500] !";
+        } else if (exception === "parsererror") {
+          msg = "Erreur de parcours dans le JSON !";
+        } else if (exception === "timeout") {
+          msg = "Erreur de délai dépassé [Time out] !";
+        } else if (exception === "abort") {
+          msg = "Requête Ajax stoppée !";
+        } else {
+          msg = "Erreur inconnue : \n" + xhr.responseText;
+        }
+        httpErrorCallbackFn(msg);
+      },
+    });
+  }
+}
---
+++ projet/projet/js/indexCtrl.js
@@ -0,0 +1,41 @@
+/*
+  But :     
+  Auteur : prénom + nom
+  Date :   jj.mm.aaaa / V1.0
+*/
+
+$().ready(function () {
+  // service et indexCtrl sont des variables globales qui doivent être accessible depuis partout => pas de mot clé devant ou window.xxx
+  http = new HttpService();
+  indexCtrl = new IndexCtrl();  // ctrl principal
+  http.centraliserErreurHttp(indexCtrl.afficherErreurHttp);
+});
+
+class IndexCtrl {
+  constructor() {
+    this.vue = new VueService();
+    this.loadLogin();
+  }
+
+  afficherErreurHttp(msg) {
+    alert(msg);
+  }
+
+  // avec arrow function
+  loadLogin() {
+    this.vue.chargerVue("login", () => new LoginCtrl());
+  }
+
+  // avec function classique
+  loadAccueil(langue) {
+    this.vue.chargerVue("accueil", function () {
+      new AccueilCtrl(langue);
+    });
+  }
+
+  loadCompte() {
+    this.vue.chargerVue("compte", function () {
+      new CompteCtrl();
+    });
+  }
+}
